e125eff332de0134496485f38705a789e636e40e
MoisesDN <m.dias009@gmail.com>
2024-03-08 12:03:58 -0300

    Project updated

--- src/main/java/org/example/aplicacao/Main.java
+++ src/main/java/org/example/aplicacao/Main.java
@@ -1,8 +1,42 @@
-package org.example.application;
+package org.example.aplicacao;
+
+import org.example.dominio.Pessoa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
 
-        System.out.println("Hello world!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("teste-jpa");
+
+        EntityManager em = emf.createEntityManager();
+        /*System.out.print("Digite um nome da Pessoa procurada: ");
+        String pessoaP = sc.nextLine();*/
+
+        Pessoa p = em.find(Pessoa.class, 2);
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
+
+        System.out.println(p);
+
+        /*em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();*/
+
+        System.out.println("Pronto!");
+        em.close();
+        emf.close();
+        sc.close();
+
     }
 }
--- src/main/java/org/example/dominio/Pessoa.java
+++ src/main/java/org/example/dominio/Pessoa.java
@@ -1,2 +1,66 @@
-package org.example.dominio;public class Pessoa {
+package org.example.dominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serial;
+import java.io.Serializable;
+
+
+@Entity
+public class Pessoa implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Pessoa setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Pessoa setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pessoa setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
